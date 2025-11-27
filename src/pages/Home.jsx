1f416b52bccfecf0aa1bd7145dbcd93c88a158a2
awais7796 <awaitzz49@gmail.com>
2025-11-27 16:10:21 +0530

    disolve card to make wavy bg full screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import './Home.css'
 function Home() {
   return (
     <div className="home">
-      <section className="hero hscreen">
+      <section className="hero">
         <WavyBackground
           containerClassName="hero-wavy"
           className="hero-overlay"
